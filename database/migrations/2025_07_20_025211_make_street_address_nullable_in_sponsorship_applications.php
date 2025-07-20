2d1112c56223b855f66555443b0bfc76ce59b5d9
Alvin Chua <alvin.chua@importcrate.com>
2025-07-19 22:00:41 -0500

    Make stree_address nullable on sponsorship_applications table again

--- database/migrations/2025_07_20_025211_make_street_address_nullable_in_sponsorship_applications.php
+++ database/migrations/2025_07_20_025211_make_street_address_nullable_in_sponsorship_applications.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('sponsorship_applications', function (Blueprint $table) {
-            //
+            $table->string('street_address')->nullable()->change();
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('sponsorship_applications', function (Blueprint $table) {
-            //
+            $table->string('street_address')->nullable(false)->change();
         });
     }
 };
